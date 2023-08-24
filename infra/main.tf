05dc8316ec9ce6d6246601711c99e34728f22fe1
Adauto Júnior <dev.adautojunior@gmail.com>
2023-08-24 11:12:10 -0300

    finished class 02

--- infra/main.tf
+++ infra/main.tf
@@ -2,14 +2,13 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 3.27"
+      version = "~> 4.16"
     }
   }
 
-  required_version = ">= 0.14.9"
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  profile = "default"
-  region = "us-west-2"
+  region = "us-east-1"
 }
